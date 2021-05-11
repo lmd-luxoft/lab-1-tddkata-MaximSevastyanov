f8511a297b25769e338049fb27d7cd737eb5ba72
Maxim Sevastyanov <msevan93@gmail.com>
2021-05-11 13:17:05 +0300

    шаг1

--- TDDKata/TestClass.cs
+++ TDDKata/TestClass.cs
@@ -1,5 +1,6 @@
 ﻿// NUnit 3 tests
 // See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
@@ -15,6 +16,20 @@ namespace TDDKata
             StringCalc calc = new StringCalc();
             int value = calc.Sum("2,2");
             Assert.That(value, Is.EqualTo(4), "Wrong actual value");
+        }
+        [Test]
+        public void EmptyValueTest()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum("");
+            Assert.That(value, Is.EqualTo(0), "Wrong result. Should be zero.");
+        }
+        [Test]
+        public void NegativeValueTest()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum("abc");
+            Assert.That(value, Is.EqualTo(expected: typeof(ArgumentException)), "Wrong argument.");
         }
     }
 }
