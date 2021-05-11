6ac852bd1c904f032ea6934610716c6adefdba41
Maxim Sevastyanov <msevan93@gmail.com>
2021-05-11 13:31:08 +0300

    попытка2

--- TDDKata/TestClass.cs
+++ TDDKata/TestClass.cs
@@ -1,5 +1,6 @@
 ﻿// NUnit 3 tests
 // See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
@@ -16,5 +17,34 @@ namespace TDDKata
             int value = calc.Sum("2,2");
             Assert.That(value, Is.EqualTo(4), "Wrong actual value");
         }
+        [Test]
+        public void EmptyValueTest()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum("");
+            Assert.That(value, Is.EqualTo(0), "Wrong result. Should be zero.");
+        }
+        [Test]
+        public void WrongArgumentTest()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum("abc");
+            Assert.That(value, Is.EqualTo(expected: typeof(ArgumentException)), "Wrong argument.");
+        }
+        [Test]
+        public void NegativeValueTest()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum("-1,0");
+            Assert.That(value, Is.LessThan(0), "Not negative value.");
+        }
+        [Test]
+        public void MoreThanTwoNumbersTest()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum("0,0,0");
+            Assert.That(value, Is.EqualTo(expected: typeof(IndexOutOfRangeException)), "More than two numbers.");
+        }
+
     }
 }
